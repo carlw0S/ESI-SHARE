3b9769d649135e2aaadd0c4ff75ffcbeb554bc9f
Francisco J. Sanchez <zheoni@outlook.es>
2018-04-09 15:50:16 +0200

    Comentado el modulo de control de inicio

--- control_inicio.c
+++ control_inicio.c
@@ -4,6 +4,7 @@ int login(T_Usuarios * usuarios, int N){
   char in_user[6], in_pass[9];
   int i, encontrado, correcto, id;
 
+  //pido al usuario que introduzca los datos de inicio de sesion
   printf("Inicie sesion:\n");
   printf("  User: ");
   fflush(stdin);
@@ -12,142 +13,144 @@ int login(T_Usuarios * usuarios, int N){
   fflush(stdin);
   fgets(in_pass, 9, stdin);
 
+  //recorro el vector de los usuarios
   for(i=0, encontrado=0, correcto=0; i<N && encontrado==0; i++){
-    if(strcmp(in_user, usuarios[i].User)==0){
-      encontrado=1;
-      if(strcmp(in_pass, usuarios[i].Login)==0){
-        correcto=1;
-        id=usuarios[i].Id_usuario;
+    if(strcmp(in_user, usuarios[i].User)==0){     //si existe el usuario:
+      encontrado=1;                 //marco para salir en la siguiente iteracion
+      if(strcmp(in_pass, usuarios[i].Login)==0){  //pruebo la contraseña
+        correcto=1;                               //marco que es correcta
+        id=usuarios[i].Id_usuario;                //guardo la id del usuario
       }
     }
   }
 
-  if(encontrado==1){
-    if(correcto==1){
-      return id;
-    }else return -1;
-  }else return -2;
+  if(encontrado==1){                 //si el usuario existe:
+    if(correcto==1){                      //si la contraseña es correcta
+      return id;                              //devuelvo la id del usuario
+    }else return -1;                      //si no, devuelvo -1
+  }else return -2;                   //si no, devuelvo -2
 }
 
 void pantalla_err_login(T_Usuarios * usuarios, int N, int * id){
   int o;
   int err=0;
-  do {
-    if (*id == -1){
+  do {      //repito mientras la id sea incorrecta (0 marca salida del programa)
+    if (*id == -1){       //en el caso de que la contraseña sea incorrecta
       puts("\n**PASS INCORRECTA**");
 
       do{
-        err=0;
-        puts("Que desea hacer?");
+        err=0;                                //presento el menu y guardo la
+        puts("Que desea hacer?");             //opcion del usuario
         puts("  1.Volver a intentar");
         puts("  2.Salir");
 
         fflush(stdin);
         scanf("%i", &o);
 
-        switch (o) {
+        switch (o) {                          //segun sea esta opcion:
           case 1: {
             puts("\n");
-            *id=login(usuarios, N);
+            *id=login(usuarios, N);           //vuevlo a intentar el login
           }
           break;
 
           case 2: {
-            *id=0;
+            *id=0;                            //marco la id para salir
           }
           break;
 
           default: {
-            err=1;
+            err=1;                            //si lo introducido no es correcto
             puts("**Esta opcion no existe**");
           }
           break;
         }
-      }while(err==-1);
+      }while(err==-1);   //repito esto mientras el usuario no elija algo posible
 
-    }else{puts("\n**EL USUARIO NO EXISTE**");
+    }else{puts("\n**EL USUARIO NO EXISTE**");   //si el usuario no existe:
 
     do{
-      err=0;
+      err=0;                                  //presento el menu
       puts("Que desea hacer?");
       puts("  1.Volver a intentar");
       puts("  2.Crer un nuevo usuario");
       puts("  3.Salir");
 
-      fflush(stdin);
+      fflush(stdin);                          //guardo la opcion del usuario
       scanf("%i", &o);
 
-      switch (o) {
+      switch (o) {                            //segun sea la opcion:
         case 1: {
-          err=0;
           puts("\n");
-          *id=login(usuarios, N);
+          *id=login(usuarios, N);             //vuelvo a intentar el login
         }
         break;
 
         case 2: {
-          puts("crearusuario");//crear_usuario();
+          puts("crearusuario");//crear_usuario();   //creo un nuevo usuario
           puts("\n");
-          *id=login(usuarios, N);
+          *id=login(usuarios, N);              //y vuelvo a intentar el login
         }
         break;
 
         case 3: {
-          *id=0;
+          *id=0;                              //marco la id para la salida
         }
         break;
 
         default: {
-          err=1;
+          err=1;                              //si la opcion no es correcta
           puts("**Esta opcion no existe**");
         }
         break;
       }
-    }while(err==1);
+    }while(err==1);       //repito mientras las opcion no sea correcta
 
     }
-  } while(*id < 0);
+  } while(*id < 0);       //repito todo esto mientras la id sea incorrecta y no
+                          //este marcada para salida con un 0
 }
 
 int menu_principal(T_Usuarios * usuarios, int N, int id){
 
   int i, pos, f, o, perfil;
 
-  for(i=0, f=0; i<N&&f==0; i++){
+  for(i=0, f=0; i<N&&f==0; i++){          //busco la posicion segun la id
     if (usuarios[i].Id_usuario==id) {
       pos=i;
       f=1;
     }
   }
 
-  printf("%s\n", usuarios[pos].Nomb_usuario);
+  printf("%s\n", usuarios[pos].Nomb_usuario);    //imprimo el nombre de usuario
 
+  //segun sea admin o user
   if (strcmp(usuarios[pos].Perfil_usuario, "usuario")==0){
-    perfil=1;
-    puts("\nElija una opcion: ");
+    perfil=1;                       //si es user la salida sera positiva
+    puts("\nElija una opcion: ");   //menu para user
     puts("  1.Perfil");
     puts("  2.Mis vehiculos");
     puts("  3.Viajar");
     puts("  4.Incidencias");
     puts("  5.Salir");
-    fflush(stdin);
+    fflush(stdin);            //tomo la opcion elegida
     scanf("%i", &o);
   }else{
-    perfil=-1;
-    puts("\nElija una opcion: ");
+    perfil=-1;                      //si es admin la salida sera negativa
+    puts("\nElija una opcion: ");     //menu para admin
     puts("  1.Usuarios");
     puts("  2.Vehiculos");
     puts("  3.Viajes");
     puts("  4.Incidencias");
     puts("  5.Salir");
-    fflush(stdin);
+    fflush(stdin);                    //tomo la opcion elegida
     scanf("%i", &o);
   }
-  return o*perfil;
+  return o*perfil;            //devuelvo la opcion con el correspondiente signo
 }
 
 void titulo(){
-  puts("---------------------------------");
+  puts("---------------------------------");    //imprime el titulo del programa
   puts("|           ESI-SHARE           |");
   puts("---------------------------------");
 }
@@ -155,27 +158,30 @@ void titulo(){
 void check_usuario_bloqueado(T_Usuarios * usuarios, int N, int * id){
   int i, f, pos;
 
-  for(i=0, f=0; i<N&&f==0; i++){
+  for(i=0, f=0; i<N&&f==0; i++){          //hallo la posicion segun la id
     if (usuarios[i].Id_usuario==*id) {
       pos=i;
       f=1;
     }
   }
 
-  if(strcmp(usuarios[pos].Estado, "bloqueado")){
+  if(strcmp(usuarios[pos].Estado, "bloqueado")){   //compruebo si esta bloqueado
     puts("**Este usuario esta bloqueado**");
-    *id = 0;
+    *id = 0;            //marco la id para salida si lo esta
   }
 }
 
-void rm_fin_linea(char *str, int n){
+char * rm_fin_linea(char *str, int n){
   int i, salida;
   char aux;
+  //recorro la cadena hasta el final o hasta la marca de salida
   for (i=0, salida=0; i<n && salida==0; i++) {
-    aux=str[i];
-    if (aux=='\n') {
-      str[i] = '\0';
-      salida = 1;
-    }else if (i==(n-1)) str[i] = '\0';
+    aux=str[i];         //voy tomando los caracteres de la cadena
+    if (aux=='\n') {    //si es '\n':
+      str[i] = '\0';        //lo sustituyo por un '\0'
+      salida = 1;           //marco para la salida
+    }else if (i==(n-1)) str[i] = '\0';    //si no, en el ultimo caracter pongo
+                                          // '\0' por si acaso
   }
+  return str;
 }
--- control_inicio.h
+++ control_inicio.h
@@ -4,17 +4,10 @@
 #include <string.h>
 #include <stdio.h>
 
-#ifndef COMPROBACION_HORAS_VIAJES_H
-#define COMPROBACION_HORAS_VIAJES_H
+#ifndef check_HORAS_VIAJES_H
+#define check_HORAS_VIAJES_H
 
-void comprobacion_horas_viajes(T_Viajes *);
-#endif
-
-
-#ifndef COMPROBACION_bloqueado_H
-#define COMPROBACION_INCIDENCIAS_H
-
-void comprobacion_bloqueado(T_Usuarios *);
+void check_horas_viajes(T_Viajes *);
 #endif
 
 #ifndef LOGIN_H
@@ -35,32 +28,62 @@ int login(T_Usuarios *, int );
 #ifndef PANTALLA_ERR_LOGIN_H
 #define PANTALLA_ERR_LOGIN_H
 /*
-Cabecera void pantalla_login_h(T_Usuarios * usuarios, int N, int * id);
+Cabecera: void pantalla_login_h(T_Usuarios * usuarios, int N, int * id);
 
 Precondicion: Recibe un vector relleno de T_Usuarios, la cantida de
       elementos de este y la id (o resultado fallido de login()) del usuario
 
 Postcondicion: Permite al usuario volver a intentar iniciar sesion, salir
-      del programa o crear un nuevo usuario.
+      del programa o crear un nuevo usuario. Si desea salir, la id del usuario
+      valdra 0.
 */
 void pantalla_err_login(T_Usuarios *, int , int *);
 #endif
 
+
 #ifndef MENU_PRINCIPAL_H
 #define MENU_PRINCIPAL_H
+/*
+Cabecera: int menu_principal(T_Usuarios *, int N, int id);
 
+Precondicion: recibe un vector de T_Usuarios cargado, la cantidad de usuarios y
+      la id del usuario actual.
+
+Postcondicion: imprime el menu adecuado a el usuario actual y devuelve:
+      >El signo del numero dice si es un usuario o un administrador:
+                + => usuario        - => administrador
+      >1 para el/los perfil(es)
+      >2 para el/los vehiculo(s)
+      >3 para el/los viaje(s)
+      >4 para el/las incidencia(s)
+      >5 para salir
+*/
 int menu_principal(T_Usuarios *, int, int);
 #endif
 
 #ifndef TITULO_H
 #define TITULO_H
+/*
+Cabecera: void titulo();
 
+Precondicion:
+
+Postcondicion: imprime el titulo del programa
+*/
 void titulo();
 #endif
 
 #ifndef CHECK_USUARIO_BLOQUEADO_H
 #define CHECK_USUARIO_BLOQUEADO_H
+/*
+Cabecera: void check_usuario_bloqueado(T_Usuarios *, int N, int * id);
 
+Precondicion: recibe un vector cargado de T_Usuarios, la cantidad de elementos y
+      la id del usuario actual.
+
+Postcondicion: Comprueba si el usuario esta bloqueado. En tal caso imprime el
+      mensaje de error en la pantalla y cambia la id de usuario a 0.
+*/
 void check_usuario_bloqueado(T_Usuarios *, int, int*);
 #endif
 
@@ -68,6 +91,13 @@ void check_usuario_bloqueado(T_Usuarios *, int, int*);
 /*Privada*/
 #ifndef RM_FIN_LINEA_H
 #define RM_FIN_LINEA_H
+/*
+Cabecera: char * rm_fin_linea(char *, int n);
 
-void rm_fin_linea(char *, int );
+Precondicion: recibe una cadena de n caracteres
+
+Postcondicion: sustituye el primer caracter '\n' que encuentre por un '\0'.
+      Ademas devuelve la cadena.
+*/
+char * rm_fin_linea(char *, int );
 #endif
