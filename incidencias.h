7510234d15b371d58bd36d5ece79a3c67e762bf0
Fran <zheoni@outlook.es>
2018-04-18 18:08:13 +0200

    mod estructura incidencias

--- incidencias.h
+++ incidencias.h
@@ -9,6 +9,6 @@ typedef struct
   int Id_us_registra;     //Id del usuario que registra la incidecia
   int Id_us_incidencia;   //Id del usuario sobre el que recae la incidencia
   char Desc_incidencia[101];  //Descripcion de la incidencia, max 100 char.
-  char Est_incidencia[9]; //Estado de la incidencia (Abierta, Validada, Cerrada)
+  char Est_incidencia[9]; //Estado de la incidencia (abierta, validada, cerrada)
 } T_Incidencias;
 #endif
